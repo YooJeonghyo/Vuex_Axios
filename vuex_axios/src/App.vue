acb7bb22802660601081a2ee74ef8ac0e735e61c
YooJeonghyo <yjh5166@gmail.com>
2022-02-22 13:47:16 +0900

    회원가입 및 회원목록 관리 구현 default
    네비게이터 기능 구현
    Users 페이지에서 SingUp 컴포넌트에 회원 정보 입력 시 AllUsers 컴포넌트에서 목록 표출

---
+++ vuex_axios/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title> 타이틀 </v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list-item v-for="item in items" :key="item.title" :to="item.to">
+        <v-list-item-content>
+          {{ item.title }}
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-footer app color="indigo" dark absolute>
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      items: [
+        {
+          title: 'home',
+          to: '/'
+        },
+        {
+          title: 'about',
+          to: '/about'
+        },
+        {
+          title: 'users',
+          to: '/users'
+        }
+      ]
+    };
+  },
+  props: {
+    source: String
+  }
+};
+</script>
---
+++ vuex_axios/src/components/Users/AllUsers.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>All Users</h1>
+    <v-list two-line>
+      <v-list-item v-for="(user, index) in allUsers" :key="index">
+        <v-list-item-avatar color="grey lighten-3">
+          <img :src="user.src" />
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-html="user.name"></v-list-item-title>
+          <v-list-item-subtitle>id:#{{ index }} / {{ user.address }} 거주</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import { EventBus } from '@/main.js';
+
+export default {
+  data() {
+    return {
+      allUsers: [
+        { userId: 'test1', password: '123', name: 'test1', address: 'Seoul', src: 'https://octodex.github.com/images/yogitocat.png' },
+        { userId: 'yjh', password: '456', name: 'yjh', address: 'Berlin', src: 'https://octodex.github.com/images/Fintechtocat.png' },
+        { userId: 'vuex', password: '789', name: 'vuex', address: 'Busan', src: 'https://octodex.github.com/images/boxertocat_octodex.jpg' }
+      ]
+    };
+  },
+  mounted() {
+    EventBus.$on('signUp', users => {
+      this.allUsers.push(users);
+    });
+  }
+};
+</script>
