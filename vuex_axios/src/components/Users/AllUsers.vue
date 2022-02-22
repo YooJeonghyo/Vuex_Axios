4d68b05fc7aa0bf3d3da9c2a37b01ce5d61189d7
YooJeonghyo <yjh5166@gmail.com>
2022-02-22 15:17:24 +0900

    state, getters 정리
    mapState, mapGetters를 통한 코드 정리

--- vuex_axios/src/components/Users/AllUsers.vue
+++ vuex_axios/src/components/Users/AllUsers.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1>All Users</h1>
+    <h1>All Users({{ count }})</h1>
+    <h3>서울 거주자 : {{ seouls }} 명 ({{ percent }}%)</h3>
     <v-list two-line>
-      <v-list-item v-for="(user, index) in getAllUsers" :key="index">
+      <v-list-item v-for="(user, index) in allUsers" :key="index">
         <v-list-item-avatar color="grey lighten-3">
           <img :src="user.src" />
         </v-list-item-avatar>
@@ -16,11 +17,16 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   computed: {
-    ...mapGetters(['getAllUsers'])
+    ...mapState(['allUsers']),
+    ...mapGetters({
+      count: 'getUsersCount',
+      seouls: 'countOfSeoul',
+      percent: 'percentOfSeoul'
+    })
   },
   data() {
     return {};
